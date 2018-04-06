098f22a65aa39e075265b5e3709bb566528a02d7
Chiranjivee Thakur <chiranjiveethakur@CJs-MacBook-Pro.local>
2018-04-07 02:12:38 +0530

    Adding basic support for creation and deletion of tasks.

---
+++ app.js
@@ -0,0 +1,129 @@
+// Define UI Vars
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+    // Add task event
+    document.addEventListener('DOMContentLoaded', getTasks);
+    form.addEventListener('submit', addTask);
+    taskList.addEventListener('click', removeTask);
+    clearBtn.addEventListener('click', clearTask);
+    filter.addEventListener('keyup', filterTask);
+}
+
+// Add Task
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Add a task');
+    }
+
+    createTaskItemAndAppendToList(taskInput.value);
+    storeTaskInLocalStorage(taskInput.value);
+    taskInput.value = '';
+    e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
+        }
+    }
+}
+
+// Clear task
+function clearTask(e) {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    localStorage.clear();
+}
+
+function filterTask(e) {
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function (task) {
+        const item = task.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
+}
+
+function storeTaskInLocalStorage(task) {
+    let tasks = getExistingTasksFromLocalStorage();
+
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Gets tasks from LS
+function getTasks() {
+    let tasks = getExistingTasksFromLocalStorage();
+
+    tasks.forEach(function(task) {
+        createTaskItemAndAppendToList(task);
+    });
+}
+
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks = getExistingTasksFromLocalStorage();
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task) {
+            tasks.splice(index, 1);
+        }
+    })
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getExistingTasksFromLocalStorage() {
+    let tasks;
+
+    if (localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    
+    return tasks;
+}
+
+function createTaskItemAndAppendToList(taskValue) {
+    // Create li element
+    const li = document.createElement('li');
+
+    // Add class
+    li.className = 'collection-item';
+
+    // Create text node and append to li
+    li.appendChild(document.createTextNode(taskValue));
+
+    // Create new link element
+    const link = document.createElement('a');
+
+    // Add class
+    link.className = 'delete-item secondary-content';
+
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+
+    // Append the link to li
+    li.appendChild(link);
+
+    // Append li to ul
+    taskList.appendChild(li);
+}
